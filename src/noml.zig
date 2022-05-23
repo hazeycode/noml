e41bf00ad7b527ec104dd2e43b1d09c64775ca77
hazeycode <22148308+hazeycode@users.noreply.github.com>
2022-05-23 17:35:36 +0100

    boolean expressions wip, test fails

--- src/noml.zig
+++ src/noml.zig
@@ -2,17 +2,22 @@ const std = @import("std");
 
 const Expression = struct {
     data: union(enum) {
-        @"error": ParseError,
         identifier: []const u8,
         string: []const u8,
+        number: Rational,
+        boolean: bool,        
+        list: void,
         unary_op_neg: void,
+        unary_op_not: void,
         binary_op_add: void,
         binary_op_sub: void,
         binary_op_mul: void,
         binary_op_div: void,
         binary_op_eql: void,
-        number: Rational,
-        list: void,
+        binary_op_and: void,
+        binary_op_or: void,
+        binary_op_not: void,
+        @"error": ParseError,
     },
     token: ?Token = null,
     next: ?*Expression = null,
@@ -26,6 +31,7 @@ const Rational = struct {
 const ParseError = error{
     UnexpectedToken,
     MalformedNumberLiteral,
+    MalformedBooleanLiteral,
 };
 
 const Token = struct {
@@ -38,6 +44,7 @@ const Token = struct {
         identifier,
         string,
         number,
+        boolean,
         paren_open,
         paren_close,
         list_open,
@@ -52,6 +59,11 @@ const Token = struct {
         keyword_using,
         keyword_let,
         keyword_in,
+        keyword_true,
+        keyword_false,
+        keyword_and,
+        keyword_or,
+        keyword_not,
     };
 
     const map = std.ComptimeStringMap(Tag, .{
@@ -69,12 +81,19 @@ const Token = struct {
         .{ "using", .keyword_using },
         .{ "let", .keyword_let },
         .{ "in", .keyword_in },
+        .{ "true", .keyword_true },
+        .{ "false", .keyword_false },
+        .{ "and", .keyword_and },
+        .{ "or", .keyword_or },
+        .{ "not", .keyword_not },
     });
 
     pub fn precedence(self: @This()) u32 {
         return switch (self.tag) {
             .plus, .minus => 10,
             .times, .divide => 11,
+            .keyword_or => 20,
+            .keyword_and => 21,
             else => 0,
         };
     }
@@ -103,8 +122,8 @@ const Tokeniser = struct {
                 .tag = tag,
             };
         }
-       
-        if (std.ascii.isAlpha(self.bytes[self.cursor])) return self.identifier()
+              
+        if (std.ascii.isAlpha(self.bytes[self.cursor])) return self.keywordOrIdentifier()
         else if (self.bytes[self.cursor] == '\"') return self.string()
         else if (std.ascii.isDigit(self.bytes[self.cursor])) return self.number()
         else {
@@ -117,7 +136,7 @@ const Tokeniser = struct {
         }
     }
     
-    fn identifier(self: *@This()) Token {
+    fn keywordOrIdentifier(self: *@This()) Token {
         var forward_cursor = self.cursor + 1;
         
         while (forward_cursor < self.bytes.len) : (forward_cursor += 1) {
@@ -133,10 +152,12 @@ const Tokeniser = struct {
         
         defer self.cursor = forward_cursor;
         
+        const len = forward_cursor - self.cursor;
+        const tag = Token.map.get(self.bytes[self.cursor..(self.cursor + len)]) orelse .identifier;
         return Token {
             .position = self.cursor,
-            .len = forward_cursor - self.cursor,
-            .tag = .identifier,
+            .len = len,
+            .tag = tag,
         };
     }
     
@@ -269,6 +290,81 @@ fn number(source_bytes: []const u8) !Rational {
     }
 }
 
+fn boolean(source_bytes: []const u8) ParseError!bool {
+    return if (std.mem.eql(u8, source_bytes, "true")) true
+    else if (std.mem.eql(u8, source_bytes, "false")) false
+    else ParseError.MalformedBooleanLiteral;
+}
+
+fn booleanExpression(allocator: std.mem.Allocator, tokeniser: *Tokeniser, expr: Expression) anyerror!?Expression {
+    var expr0 = expr;
+    
+    if (tokeniser.next()) |next_token| {
+        var expr1 = Expression{
+            .token = next_token,
+            .data = undefined,
+        };
+        
+        switch (expr1.token.?.tag) {
+            .keyword_and, .keyword_or => {
+                expr1.data = switch (expr1.token.?.tag) {
+                    .keyword_and => .binary_op_and,
+                    .keyword_or => .binary_op_or,
+                    else => unreachable,
+                };
+                
+                if (try expression(allocator, tokeniser)) |expr2| {
+                    switch (expr2.token.?.tag) {
+                        .keyword_and, .keyword_or => {
+                            if (expr2.token.?.precedence() < expr1.token.?.precedence()) {
+                                const rhs = expr2.next.?.next;
+                                const temp_lhs = expr2.next.?.*;
+    
+                                expr1.next = try allocator.create(Expression);
+                                expr0.next = try allocator.create(Expression);
+    
+                                expr0.next.?.* = temp_lhs;
+                                expr0.next.?.next = rhs;
+    
+                                expr1.next.?.* = expr0;
+                                expr2.next.?.* = expr1;
+    
+                                return expr2;
+                            }
+                        },
+                        else => {},
+                    }
+                    expr0.next = try allocator.create(Expression);
+                    expr0.next.?.* = expr2;
+                }
+                
+                expr1.next = try allocator.create(Expression);
+                expr1.next.?.* = expr0;
+                
+                return expr1;
+            },
+            .keyword_not => {
+                expr1.data = .unary_op_not;
+                if (try expression(allocator, tokeniser)) |expr2| {
+                    expr1.next = try allocator.create(Expression);
+                    expr1.next.?.* = expr2;
+                }
+                return expr1;
+            },
+            else => {
+                expr1.data = .{ .@"error" = ParseError.UnexpectedToken };
+    
+                expr0.next = try allocator.create(Expression);
+                expr0.next.?.* = expr1;
+    
+                return expr0;
+            },
+        }
+    } else {
+        return expr0;
+    }
+}
+
 fn expression(allocator: std.mem.Allocator, tokeniser: *Tokeniser) anyerror!?Expression {
     if (tokeniser.next()) |token| {
         switch (token.tag) {
@@ -351,6 +447,24 @@ fn expression(allocator: std.mem.Allocator, tokeniser: *Tokeniser) anyerror!?Exp
                     return expr0;
                 }
             },
+            .keyword_true => {
+                return try booleanExpression(allocator, tokeniser, .{
+                    .token = token,
+                    .data = .{ .boolean = true },
+                });
+            },
+            .keyword_false => {
+                return try booleanExpression(allocator, tokeniser, .{
+                    .token = token,
+                    .data = .{ .boolean = false },
+                });
+            },
+            .keyword_not => {
+                return Expression{
+                    .token = token,
+                    .data = .unary_op_not,  
+                };
+            },
             else => {
                 return Expression{
                     .token = token,
@@ -380,6 +494,9 @@ pub fn free(allocator: std.mem.Allocator, expr: Expression) void {
     }
 }
 
+
+// TESTS ----------------------------------------------------------------------------------------------------
+
 const testing = std.testing;
 
 fn testTokenise(input: []const u8, expected: []const Token) !void {
@@ -394,6 +511,40 @@ fn testTokenise(input: []const u8, expected: []const Token) !void {
     }
     
     try testing.expectEqualSlices(Token, expected, tokens.items);
+}
+
+fn testExpectExpression(expr: *Expression, expected: *Expression) !void {
+    var maybe_expected_cur: ?*Expression = expected;
+    var maybe_cur: ?*Expression = expr;
+    while (maybe_expected_cur) |expected_cur| {
+        { // test data before token for debugging purposes
+            errdefer {
+                // if data does not match expected then log the expected and actual token positions
+                // for debugging purposes
+                if (expected_cur.token) |expected_token| {
+                    std.log.err(
+                        "expected pos {}, got {}",
+                        .{expected_token.position, maybe_cur.?.token.?.position},
+                    );
+                }
+            }
+            
+            try testing.expectEqual(expected_cur.data, maybe_cur.?.data);
+        }
+
+        if (expected_cur.token) |expected_token| {
+            // test token tag first for debugging purposes
+            try testing.expectEqual(expected_token.tag, maybe_cur.?.token.?.tag);
+            try testing.expectEqual(expected_token.position, maybe_cur.?.token.?.position);
+            try testing.expectEqual(expected_token.len, maybe_cur.?.token.?.len);
+        }
+        else {
+            try testing.expectEqual(@as(?Token, null), maybe_cur.?.token);
+        }
+        
+        maybe_expected_cur = expected_cur.next;
+        maybe_cur = maybe_cur.?.next;
+    }
 }
 
 test {
@@ -433,6 +584,21 @@ test "tokenise string literals" {
     try testTokenise(test_input, &expected_result);
 }
 
+test "tokenise boolean logic keywords" {
+    const test_input: []const u8 = "true or false and not true";
+    
+    const expected_result = .{
+        Token{ .position = 0, .len = 4, .tag = .keyword_true },
+        Token{ .position = 5, .len = 2, .tag = .keyword_or },
+        Token{ .position = 8, .len = 5, .tag = .keyword_false },
+        Token{ .position = 14, .len = 3, .tag = .keyword_and },
+        Token{ .position = 18, .len = 3, .tag = .keyword_not },
+        Token{ .position = 22, .len = 4, .tag = .keyword_true },
+    };
+    
+    try testTokenise(test_input, &expected_result);
+}
+
 test "tokenise invalid bytes" {
     const test_input: []const u8 = "$";
 
@@ -454,7 +620,7 @@ test "parse numbers" {
     }
 }
 
-test "parse expressions" {
+test "parse basic arthimetic expressions" {
     const test_input: []const u8 = "2 + 15 * 100 - 17";
 
     var expected_17 = Expression{ 
@@ -502,13 +668,65 @@ test "parse expressions" {
     try testing.expect(expr != null);
 
     defer free(testing.allocator, expr.?);
-
-    var maybe_expected_cur: ?*Expression = &expected;
-    var maybe_cur: ?*Expression = &expr.?;
-    while (maybe_expected_cur) |expected_cur| {
-        try testing.expectEqual(expected_cur.token, maybe_cur.?.token);
-        try testing.expectEqual(expected_cur.data, maybe_cur.?.data);
-        maybe_expected_cur = expected_cur.next;
-        maybe_cur = maybe_cur.?.next;
-    }
+    
+    try testExpectExpression(&expected, &expr.?);
 }
+
+test "parse boolean logic expressions" {
+    const test_input: []const u8 = "true and false or true or not true"; // lol!
+    
+    var expected_true_3rd = Expression{
+        .data = .{ .boolean = true },
+        .token = .{ .position = 30, .len = 4, .tag = .keyword_true },
+    };
+    
+    var expected_not = Expression{
+        .data = .unary_op_not,
+        .token = .{ .position = 26, .len = 3, .tag = .keyword_not },
+        .next = &expected_true_3rd,
+    };
+    
+    var expected_true_2nd = Expression{
+        .data = .{ .boolean = true },
+        .token = .{ .position = 18, .len = 4, .tag = .keyword_true },
+        .next = &expected_not,
+    };
+    
+    var expected_false = Expression{
+        .data = .{ .boolean = false },
+        .token = .{ .position = 9, .len = 5, .tag = .keyword_false },
+        .next = &expected_true_2nd,
+    };
+    
+    var expected_true_1st = Expression{
+        .data = .{ .boolean = true },
+        .token = .{ .position = 0, .len = 4, .tag = .keyword_true },
+        .next = &expected_false,
+    };
+    
+    var expected_and = Expression{
+        .data = .binary_op_and,
+        .token = .{ .position = 5, .len = 3, .tag = .keyword_and },
+        .next = &expected_true_1st,
+    };
+    
+    var expected_or_2nd = Expression{
+        .data = .binary_op_or,
+        .token = .{ .position = 23, .len = 2, .tag = .keyword_or },
+        .next = &expected_and,
+    };
+    
+    var expected = Expression{
+        .data = .binary_op_or,
+        .token = .{ .position = 15, .len = 2, .tag = .keyword_or },
+        .next = &expected_or_2nd,
+    };
+    
+    var expr = try parse(testing.allocator, test_input);
+    try testing.expect(expr != null);
+    
+    defer free(testing.allocator, expr.?);
+    
+    try testExpectExpression(&expected, &expr.?);
+}
+
